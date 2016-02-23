b8a6d66dbc67c42a4caa59d6ad3d5fea79c4ecc7
shihao <sheraton@sina.com>
2016-02-23 12:15:13 +0800

    test

--- Hello/src/com/example/hello/MainActivity.java
+++ Hello/src/com/example/hello/MainActivity.java
@@ -3,12 +3,7 @@ package com.example.hello;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -18,12 +13,8 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity implements OnClickListener{
-	Button buttonStart, buttonStop; 
-	MyService receiveMsgService;  
-     
-    // 记录当前连接状态，因为广播会接收所有的网络状态改变wifi/3g等等，所以需要一个标志记录当前状态 
-	private boolean conncetState = true;
-	
+	Button buttonStart, buttonStop; 	 
+  
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		// TODO Auto-generated method stub
@@ -39,11 +30,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		//设置监听
-		Intent intent = new Intent(MainActivity.this, MyService.class);
-		bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);  
-
-		
+				
 		//开启
 		buttonStart = (Button) findViewById(R.id.buttonstart);  
         buttonStop = (Button) findViewById(R.id.buttonstop);  
@@ -52,15 +39,48 @@ public class MainActivity extends Activity implements OnClickListener{
         buttonStop.setOnClickListener(this); 
 
 	}
-	//取消注册Receiver
+	/*
+	 * 服务不能自己运行，需要通过调用Context.startService()或Context.bindService()方法启动服务。
+	 * 这两个方法都可以启动Service，但是它们的使用场合有所不同。使用startService()方法启用服务，
+	 * 调用者与服务之间没有关连，即使调用者退出了，服务仍然运行。
+	 * 使用bindService()方法启用服务，调用者与服务绑定在了一起，调用者一旦退出，服务也就终止，
+	 * 大有“不求同时生，必须同时死”的特点。
+	 */
+	boolean isServiceStarted = false;
 	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		if (receiveMsgService != null) {
-			unbindService(serviceConnection);
-			Log.i("mylog", "执行unbind()");
+	public void onClick(View src) {
+		// TODO Auto-generated method stub
+		AlertDialog.Builder builder = new Builder(MainActivity.this);
+		
+		switch (src.getId()) {
+		case R.id.buttonstart:
+			builder.setMessage("onClick: starting service").show();			
+			
+			Intent intent = new Intent(this, MyService.class);
+		    Bundle bundle = new Bundle();
+		    bundle.putInt("MESSAGE_TYPE", MyService.MSG_HELLO);
+		    intent.putExtras(bundle);
+		    
+		    if (startService(intent) == null) return;
+		    isServiceStarted = true;
+		    
+			break;
+		case R.id.buttonstop:
+			builder.setMessage("onClick: stop service").show();
+			stopService(new Intent(this, MyService.class));
+			if (isServiceStarted) {
+				Intent intent1 = new Intent(this, MyService.class);
+				Bundle bundle1 = new Bundle();
+				bundle1.putInt("MESSAGE_TYPE", MyService.MSG_STOP_SERVICE);
+				intent1.putExtras(bundle1);
+				startService(intent1);
+				isServiceStarted = false;
+			}
+			break;
 		}
 	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -79,22 +99,4 @@ public class MainActivity extends Activity implements OnClickListener{
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	@Override
-	public void onClick(View src) {
-		// TODO Auto-generated method stub
-		AlertDialog.Builder builder = new Builder(MainActivity.this);
-		
-		switch (src.getId()) {
-		case R.id.buttonstart:
-			builder.setMessage("onClick: starting service").show();
-			startService(new Intent(this, MyService.class));
-			break;
-		case R.id.buttonstop:
-			builder.setMessage("onClick: stop service").show();
-			stopService(new Intent(this, MyService.class));
-			break;
-		}
-	}
-	
-
 }
--- Hello/src/com/example/hello/MyService.java
+++ Hello/src/com/example/hello/MyService.java
@@ -1,7 +1,5 @@
 package com.example.hello;
 
-import java.util.Date;
-import java.util.Timer;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
@@ -9,11 +7,50 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
-import android.os.Binder;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
+import android.widget.Toast;
 
 public class MyService extends Service {
-
+	//http://www.codeproject.com/Articles/822717/Beginner-s-Guide-to-Android-Services
+	// Message types to the service to display a message
+	static final int MSG_STOP_SERVICE = 0;
+	static final int MSG_HELLO = 1;
+	static final int MSG_HAPPY_BIRTHDAY = 2;
+	// Handler that receives messages from the thread
+	private final class ServiceHandler extends Handler {
+	    
+	    public ServiceHandler(Looper looper) {
+	        super(looper);
+	    }
+	    
+	    @Override
+	    public void handleMessage(Message msg) {
+	        switch (msg.what) {
+	            case MSG_STOP_SERVICE:
+	                Toast.makeText(getApplicationContext(), "Service is shutting down...", Toast.LENGTH_SHORT).show();
+	                stopSelf(msg.arg1);
+	                break;
+	            case MSG_HELLO:
+	                Toast.makeText(getApplicationContext(), "Hello, Code Project! Greeting from Android Service.", Toast.LENGTH_SHORT).show();
+	                break;
+	            case MSG_HAPPY_BIRTHDAY:
+	                Toast.makeText(getApplicationContext(), "Happy Birthday to you!", Toast.LENGTH_SHORT).show();
+	                break;
+	            default:
+	                super.handleMessage(msg);
+	        }
+	    }
+	}
+	private ServiceHandler handler;
+	
+	
+	
+	
 	@Override
 	public IBinder onBind(Intent arg0) {
 		// TODO Auto-generated method stub
@@ -28,33 +65,30 @@ public class MyService extends Service {
 			String action = intent.getAction();  
             if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION))  
             {  
-                Timer timer = new Timer();  
-                timer.schedule(new MyTimerTask(getApplicationContext(),connectState), new Date());  
+                //Timer timer = new Timer();  
+                //timer.schedule(new MyTimerTask(getApplicationContext(),connectState), new Date());
+            	Toast.makeText(getApplicationContext(), "Hello, ConnectivityManager.CONNECTIVITY_ACTION! Greeting from Android Service.", Toast.LENGTH_SHORT).show();
+           
             }  
 		}  
     }; 
     
-    public interface ConnectState  
-    {          
-        // 网络状态改变之后，通过此接口的实例通知当前网络的状态，此接口在Activity中注入实例对象  
-    	public void GetState(boolean isConnected); 
-    }  
-  
-    private ConnectState connectState;  
-  
-    public void setConnectState(ConnectState connectState)  
-    {  
-        this.connectState = connectState;  
-    }  
-  
-    private Binder binder = new MyBinder();  
-    private boolean isContected = true; 
+   
 	@Override
 	public void onCreate() {
 		// 注册广播  
         IntentFilter mFilter = new IntentFilter();  
         mFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION); // 添加接收网络连接状态改变的Action  
         registerReceiver(mReceiver, mFilter); 
+        //
+        Toast.makeText(getApplicationContext(), "Service is starting...", Toast.LENGTH_SHORT).show();
+
+        HandlerThread thread = new HandlerThread("StartedService", Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+        
+        // Get the HandlerThread's Looper and use it for our Handler
+        Looper looper = thread.getLooper();
+        handler = new ServiceHandler(looper);
 	}
 	@Override
 	public void onDestroy() {
@@ -62,11 +96,16 @@ public class MyService extends Service {
 		super.onDestroy();
 		unregisterReceiver(mReceiver); // 删除广播
 	}
-	public class MyBinder extends Binder  
-    {  
-        public MyService getService()  
-        {  
-            return MyService.this;  
-        }  
-    } 
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Message msg = handler.obtainMessage();
+	    msg.arg1 = startId;
+	    msg.what = intent.getExtras().getInt("MESSAGE_TYPE");
+
+	    handler.sendMessage(msg);
+
+	    // Restart the service if it got killed
+	    return START_REDELIVER_INTENT;
+	}
+	
 }
